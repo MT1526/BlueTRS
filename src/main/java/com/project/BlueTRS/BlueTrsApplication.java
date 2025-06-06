700e843c29881ab3f36b79111d904ac05a9b89f6
MT1526 <estebansotosarkis@gmail.com>
2025-06-05 20:59:24 -0600

    Primer commit del proyecto

---
+++ src/main/java/com/project/BlueTRS/BlueTrsApplication.java
@@ -0,0 +1,13 @@
+package com.project.BlueTRS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueTrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlueTrsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/BlueTRS/BlueTrsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.BlueTRS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlueTrsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
